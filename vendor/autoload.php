f9dccc4632a3104869a2a0505c48e978ede61ff7
Brad Waye <bradwaye@gmail.com>
2019-12-22 09:06:01 -0500

    new site

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit0751ead4172fbf86c48806f3b09db174::getLoader();
+return ComposerAutoloaderInit8eb6309a2d23e6aa91f5b8be5cc53ac5::getLoader();
